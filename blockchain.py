a65d01811eacf92efadec8109b32adc9c1e18dd5
Shivansh Shrivastav <shiva.shri0055@gmail.com>
2026-02-14 13:20:28 +0530

    added blockchain linking and validation

--- blockchain.py
+++ blockchain.py
@@ -35,8 +35,52 @@ class Block:
         print("Hash:", self.hash)
 
 
+class Blockchain:
+    def __init__(self):
+        self.difficulty = 4
+        self.chain = [self.create_genesis_block()]
+        
+
+    def create_genesis_block(self):
+        genesis_block = Block(0, ["Genesis Block"], "0")
+        genesis_block.mine_block(self.difficulty)
+        return genesis_block
+
+    def get_latest_block(self):
+        return self.chain[-1]
+
+    def add_block(self, transactions):
+        previous_block = self.get_latest_block()
+        new_block = Block(
+            index=len(self.chain),
+            transactions=transactions,
+            previous_hash=previous_block.hash
+        )
+        new_block.mine_block(self.difficulty)
+        self.chain.append(new_block)
+
+    def is_chain_valid(self):
+        for i in range(1, len(self.chain)):
+            current = self.chain[i]
+            previous = self.chain[i - 1]
+
+            if current.hash != current.calculate_hash():
+                return False
+
+            if current.previous_hash != previous.hash:
+                return False
+
+        return True
+
+
+
 
 if __name__ == "__main__":
-    genesis_block = Block(0, ["Genesis Block"], "0")
-    genesis_block.mine_block(4)
+    my_blockchain = Blockchain()
+
+    my_blockchain.add_block(["Alice -> Bob : 5 BTC"])
+    my_blockchain.add_block(["Bob -> Charlie : 2 BTC"])
+
+    print("Is blockchain valid?", my_blockchain.is_chain_valid())
+
 
