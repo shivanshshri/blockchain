3b720180191331cf8a07b5310f51649205a1b7b0
Shivansh Shrivastav <shiva.shri0055@gmail.com>
2026-02-14 13:15:41 +0530

    Initial blockchain structure with hashing

---
+++ blockchain.py
@@ -0,0 +1,25 @@
+import hashlib
+import time
+import json
+
+
+class Block:
+    def __init__(self, index, transactions, previous_hash):
+        self.index = index
+        self.timestamp = time.time()
+        self.transactions = transactions
+        self.previous_hash = previous_hash
+        self.nonce = 0
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        block_data = {
+            "index": self.index,
+            "timestamp": self.timestamp,
+            "transactions": self.transactions,
+            "previous_hash": self.previous_hash,
+            "nonce": self.nonce
+        }
+
+        block_string = json.dumps(block_data, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
